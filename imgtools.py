a4c56576d621234731ad5a9d652092431e8499c8
vdutell <vashadutell@gmail.com>
2020-02-06 05:24:49 +0000

    fix error for image sizes

--- imgtools.py
+++ imgtools.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-def crop_ims(ims, edgesize=None, area='middle'):
+def crop_ims(ims, dims=None, area='middle'):
     '''
     funtion to resize image to smallest of all images, or edgesize is supplied.
     
@@ -10,32 +10,40 @@ def crop_ims(ims, edgesize=None, area='middle'):
         area (string): Crop from the top, middle, or bottom?
     '''
 
-    if(edgesize==None):
+    if(dims==None):
         dims = np.array([(np.shape(im)) for im in ims])
         crop_h = np.min(dims[:,0])
         crop_w = np.min(dims[:,1])
-    
-    h_crop_h = np.int(crop_h/2)
-    h_crop_w = np.int(crop_w/2)
+    else:
+        crop_h = dims[0]
+        crop_w = dims[1]
+
+    h_crop_h = np.int(crop_h//2)
+    h_crop_w = np.int(crop_w//2)
     # newims = np.ones((np.shape(ims)[0],crop_h,crop_w,3))
     newims = []
+    print(h_crop_h, h_crop_w)
     for i, im in enumerate(ims):
+        print(im.shape)
+
         # print(np.shape(im))
         im_h = np.shape(im)[0]
         im_w = np.shape(im)[1]
-        h_im_h = np.int(im_h / 2) # half image height
-        h_im_w = np.int(im_w / 2) # half image width
-        
+
+        h_im_h = np.int(im_h // 2) # half image height
+        h_im_w = np.int(im_w // 2) # half image width
+
         #check to see if we have large enough images to crop
         if(im_w < 2*crop_w):
             lr_replicates=False
-        
-        if(area=='middle'):
-            addon = [d%2for d in  np.shape(ims[0])]
-            newim = im[h_im_h - h_crop_h:h_im_h + h_crop_h+addon[0], # crop height
-                       h_im_w - h_crop_w:h_im_w + h_crop_w+addon[0]] # crop width
-            newims.append(newim)
 
+        if(area=='middle'):
+            #addon = [d%2 for d in  np.shape(ims[0])]
+            newim = im[h_im_h - h_crop_h:h_im_h + h_crop_h, # crop height
+                           h_im_w - h_crop_w:h_im_w + h_crop_w] # crop width
+            newim = newim[:crop_h,:crop_w]
+            newims.append(newim)
+                
         elif(area=='top'):
             newim = im[:crop_h, # crop height
                        h_im_w - h_crop_w-1:h_im_w + h_crop_w] # crop width
@@ -43,10 +51,10 @@ def crop_ims(ims, edgesize=None, area='middle'):
 
         elif(area=='bottom'):
             newim = im[im_h - crop_h:im_h, # crop height
-                       h_im_w - h_crop_w-1:h_im_w + h_crop_w] # crop width
+                           h_im_w - h_crop_w-1:h_im_w + h_crop_w] # crop width
             newims.append(newim)
 
         else:
             raise ValueError('area not well specified!')
-        
+
     return(newims)
